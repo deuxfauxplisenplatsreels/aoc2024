2317348df05c73dffd408e28556fcd80c9d5d8fa
deuxfauxplisenplatsreels <skrap.inc@hotmail.com>
2024-12-03 21:56:17 +0100

    day 3 part 1

--- 20241203_1.py
+++ 20241203_1.py
@@ -1,9 +1,9 @@
 import re
 
-# example_input = '03\\example_input'
-example_input = '03\\input'
+# input = '03\\example_input'
+input = '03\\input'
 
-with open(example_input, 'r', encoding='UTF-8') as file:
+with open(input, 'r', encoding='UTF-8') as file:
     sum = 0
     for line in file.readlines():
         x = re.findall("mul\(\d*,\d*\)", line)
